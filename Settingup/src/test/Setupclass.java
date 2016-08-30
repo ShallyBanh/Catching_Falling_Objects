c18ba6486913d30a79a42f99e25ebc427c4bd530
ShallyBanh <banh.shally@gmail.com>
2016-08-30 14:02:22 -0600

    Update Setupclass.java

--- Settingup/src/test/Setupclass.java
+++ Settingup/src/test/Setupclass.java
@@ -16,14 +16,14 @@ public class Setupclass extends StateBasedGame {
 
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new Setupclass("setup Test")); 
-		
+		//set the display size
 		app.setDisplayMode(800, 600, false);
-		
 		app.start();
 	}
 
 
 	public void initStatesList(GameContainer container) throws SlickException {
+		//set up my two game states
 		this.addState(new GameState());
 		this.addState(new GameOverState());
 	}
