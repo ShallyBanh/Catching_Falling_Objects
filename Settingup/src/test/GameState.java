1b3c824e7c2a3034c76c06feb208dcb77f5c7ccf
ShallyBanh <banh.shally@gmail.com>
2016-08-30 13:58:58 -0600

    Update GameState.java

--- Settingup/src/test/GameState.java
+++ Settingup/src/test/GameState.java
@@ -15,7 +15,7 @@ import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
 public class GameState extends BasicGameState {
-	
+	//init of game variables
 	private ArrayList<Circle> balls;
 	private ArrayList<Circle> bombs;
 	private Circle mouseBall;
@@ -24,6 +24,7 @@ public class GameState extends BasicGameState {
 	private int life;
 	private int points;
 	private int counter;
+	//initializes out game varibales
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		balls = new ArrayList<Circle>();
 		bombs = new ArrayList<Circle>();
@@ -36,17 +37,16 @@ public class GameState extends BasicGameState {
 	}
 
 	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
-		if(container.getInput().isKeyPressed(Input.KEY_1)){
-			sbg.enterState(1, new FadeOutTransition(), new FadeInTransition());
-		}
+		//set the center of the yellow ball as the position of the mouse
 		mouseBall.setCenterX(container.getInput().getMouseX());
 		mouseBall.setCenterY(container.getInput().getMouseY());
-		
 		timePassed += delta;
 		//spawn balls every 200 milisecs
 		if(timePassed>200){
 			timePassed = 0;
+			//add a new ball
 			balls.add(new Circle(10 + random.nextInt(700),0,20));
+			//spawn a bomb for every 5 ball
 			if(counter==5){
 				bombs.add(new Circle(10 + random.nextInt(700),0,20));
 				counter = 0;
@@ -57,12 +57,15 @@ public class GameState extends BasicGameState {
 		for(Circle c: balls){
 			c.setCenterY(c.getCenterY() + (delta)/5f);
 		}
+		//check balls from the end of the array
 		for(int i = balls.size()-1; i>=0; i--){
 			Circle c = balls.get(i);
+			//checks if ball falls off the screen
 			if(c.getCenterY() > 610){
 				balls.remove(i);
 				life -= 1;
 			}
+			//check if we caught a ball
 			else if(c.intersects(mouseBall)){
 				balls.remove(i);
 				points += 1;
@@ -72,13 +75,16 @@ public class GameState extends BasicGameState {
 		for(Circle c: bombs){
 			c.setCenterY(c.getCenterY() + (delta)/5f);
 		}
+		//checks the bombs array from the end
 		for(int i = bombs.size()-1; i>=0; i--){
 			Circle c = bombs.get(i);
+			//check is collision with the bomb is made
 			if(c.intersects(mouseBall)){
 				bombs.remove(i);
 				life -= 1;
 			}
 		}
+		//enter the game over state
 		if(life==0){
 			sbg.enterState(1, new FadeOutTransition(), new FadeInTransition());
 		}
@@ -103,7 +109,6 @@ public class GameState extends BasicGameState {
 
 
 	public int getID() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
