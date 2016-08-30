e428e9b8ff123be8be54643beae99a939b3597df
ShallyBanh <banh.shally@gmail.com>
2016-08-30 13:54:41 -0600

    Update GameOverState.java

--- Settingup/src/test/GameOverState.java
+++ Settingup/src/test/GameOverState.java
@@ -9,22 +9,21 @@ import org.newdawn.slick.state.StateBasedGame;
 public class GameOverState extends BasicGameState {
 
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
-		// TODO Auto-generated method stub
 		
 	}
 	
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics g) throws SlickException {
+		//draw gameover
 		g.drawString("GAME OVER", 300, 300);
 	}
 
 
 	public int getID() {
-		// TODO Auto-generated method stub
+		//signals that we are at the gameover state
 		return 1;
 	}
 
